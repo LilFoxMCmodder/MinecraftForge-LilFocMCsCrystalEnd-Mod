835c413d267ecaffb7a251df457048b9daac5e4e
LilFoxMCmodder <fbi0101fbi@gmail.com>
2025-07-24 00:09:51 -0700

    Added crystal ore added the basic item files for the crystal ore for the SwordMod

---
+++ src/main/java/net/flickt/swordmod/Moditems.java
@@ -0,0 +1,22 @@
+package net.flickt.swordmod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SwordMod.MOD_ID);
+
+    public static final RegistryObject<Item> CRYSTALORE = ITEMS.register("crystalore", () -> new Item(new Item.Properties()));
+
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
--- src/main/java/net/flickt/swordmod/SwordMod.java
+++ src/main/java/net/flickt/swordmod/SwordMod.java
@@ -1,6 +1,7 @@
 package net.flickt.swordmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +30,7 @@ public class SwordMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        Moditems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -42,6 +44,9 @@ public class SwordMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(Moditems.CRYSTALORE);
+        }
 
     }
 
